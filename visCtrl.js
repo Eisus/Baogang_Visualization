adbd791f8bd1e357d5626928f79e96d6cdecba8b
lixueyu <susie_lxyu@163.com>
2017-10-17 20:23:58 +0800

    init

---
+++ visCtrl.js
@@ -0,0 +1,16 @@
+/**
+ * Created by LI Xueyu on 10/17/17.
+ */
+var app = angular.module('dataVis', ['ngRoute']);
+
+// app.config('$routeProvider', function($routeProvider) {
+//     $routeProvider.when('/car', { templateUrl: 'car.html' })
+//         .when('/area', { templateUrl: 'area.html' })
+//         .when('/psr', { templateUrl: 'psr.html' })
+// });
+
+
+
+app.controller('visualCtrl',['$scope', '$http', function($scope, $http) {
+
+}]);
