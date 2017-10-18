6698aa9190471da894b2ff91643c71b05af3143a
lixueyu <susie_lxyu@163.com>
2017-10-18 13:52:55 +0800

    seperate template

--- visCtrl.js
+++ visCtrl.js
@@ -1,16 +1,9 @@
 /**
  * Created by LI Xueyu on 10/17/17.
  */
-var app = angular.module('dataVis', ['ngRoute']);
-
-// app.config('$routeProvider', function($routeProvider) {
-//     $routeProvider.when('/car', { templateUrl: 'car.html' })
-//         .when('/area', { templateUrl: 'area.html' })
-//         .when('/psr', { templateUrl: 'psr.html' })
-// });
-
-
+var app = angular.module('dataVis', []);
 
 app.controller('visualCtrl',['$scope', '$http', function($scope, $http) {
+    $scope.temp = 123;
 
 }]);
